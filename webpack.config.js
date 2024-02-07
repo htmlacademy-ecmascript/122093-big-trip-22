6d906871b90a292993775c65b9df4dc891af5481
Dula53 <kad.keeper@gmail.com>
2024-02-07 13:38:55 +0600

    Разделяй и властвуй

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.js', // Точка входа
   output: {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
+    filename: 'bundle.[contenthash].js', // Имя бандла
+    path: path.resolve(__dirname, 'build'), // Директория для файлов сборки
+    clean: true, // Удаляем предыдущую сборку перед созданием новой
   },
-  devtool: 'source-map',
+  devtool: 'source-map', // Генерируем карту исходного кода
   plugins: [
     new HtmlPlugin({
       template: 'public/index.html',
@@ -26,7 +26,7 @@ module.exports = {
     }),
   ],
   module: {
-    rules: [
+    rules: [ // Добавляем лоадеры
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
@@ -37,6 +37,10 @@ module.exports = {
           },
         },
       },
-    ]
-  }
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
 };
